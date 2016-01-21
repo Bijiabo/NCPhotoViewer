35a8a80d7b770f2a875a1dd27ca5cb75ca6b93cf
Bijiabo <bijiabo@gmail.com>
2016-01-21 22:01:20 +0800

    Initial Commit

---
+++ NCPhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCPhotoViewer
+//
+//  Created by huchunbo on 16/1/21.
+//  Copyright © 2016年 Bijiabo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ NCPhotoViewerTests/NCPhotoViewerTests.swift
@@ -0,0 +1,36 @@
+//
+//  NCPhotoViewerTests.swift
+//  NCPhotoViewerTests
+//
+//  Created by huchunbo on 16/1/21.
+//  Copyright © 2016年 Bijiabo. All rights reserved.
+//
+
+import XCTest
+@testable import NCPhotoViewer
+
+class NCPhotoViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
